a28f7024277287736f65f3b43f1c85b5180961a8
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-07-13 16:40:31 -0400

    feat: table relation complete feat: .env file created to store postgres link

--- db/db.js
+++ db/db.js
@@ -1,9 +1,10 @@
+//import 'dotenv/config'
 const { Sequelize } = require("sequelize");
 const { name } = require("../package.json");
 
 //knd = `postgres://knd:2782001knd@localhost:5434/${name}`
 //connec with the database
-const db = new Sequelize(`postgres://knd:2782001knd@localhost:5434/${name}`, {
+const db = new Sequelize( process.env.POSTGRES_URL+`${name}`, {
     logging: false
 });
 
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const db = require('./db');
 const PORT = 8000;
+
 
 // Middleware
 
@@ -20,7 +22,7 @@ const startServer = async (app,port) => {
         console.log(`Server is running on port ${PORT}`);
       });
     return app;
-}
+};
 const configureApp = (port) => {
     const app = express();
     setupMiddleWare(app)
