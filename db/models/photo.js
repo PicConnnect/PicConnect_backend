f507212bb11ea352b92fb9d38565b0990e656af7
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-07-13 14:14:21 -0400

    feat: created model folder under db and set up intro table design for photo table in photo.js under model

---
+++ db/models/photo.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const Photo = db.define("photo", {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    downloads: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    urls: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+    /*
+    likes from like
+    likes_by_user from like
+    camera_details_id from camera-details
+    location_id from location
+    user_id from user
+    */
+});
+
+module.exports = Photo
