a8205f40490cc63d0b20ec0eba84629d8789bbde
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-07-15 20:55:49 -0400

    feat: remove follower using both user ID and followerID

--- api/users.js
+++ api/users.js
@@ -27,11 +27,31 @@ router.post("/:userID/addFollower/:followerID", async (req, res, next) => {
         const followed = await user.addFollower_id(follower);
         followed
             ? res.status(200).json(followed)
-            : res.status(404).send("Followed Unsuccessful")
+            : res.status(404).send("Followed Unsuccessful");
     } catch (error) {
         next(error);
     }
 });
 
+router.delete("/:userID/deleteFollower/:followerID", async (req, res, next) => {
+    const { userID, followerID } = req.params;
+    try{
+        const user = await User.findByPk(userID);
+        const follower = await User.findByPk(followerID);
+        const isFollower = await user.hasFollower_id(follower);
+
+        if(!isFollower){
+            return res.status(404).send("Follower not found for this user");
+        }
+
+        const deleteFollower = await user.removeFollower_id(follower);
+        deleteFollower
+            ? res.status(200).send("Follower deleted")
+            : res.status(400).send("Follower not deleted"); 
+
+    } catch (error) {
+        next (error);
+    }
+})
 
 module.exports = router;
