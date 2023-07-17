95a634aba6bd35a71ebca101469944df0bfdd465
woodley <gelinw3@gmail.com>
2023-07-17 11:07:24 -0400

    refactor: require dotenv

--- db/db.js
+++ db/db.js
@@ -1,8 +1,9 @@
-//import 'dotenv/config'
+require('dotenv').config();
 const { Sequelize } = require("sequelize");
 const { name } = require("../package.json");
 
 //connect with the database
+console.log("this is link",process.env.POSTGRES_URL+`${name}`)
 const db = new Sequelize(process.env.POSTGRES_URL+`${name}`, {
     logging: false
 });
