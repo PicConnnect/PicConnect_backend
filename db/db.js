5bd9bbb6142af8c654d067ac03b2eecdfdcd63ab
woodley <gelinw3@gmail.com>
2023-07-17 11:09:08 -0400

    refactor: remove console log for postgres URL

--- db/db.js
+++ db/db.js
@@ -3,7 +3,6 @@ const { Sequelize } = require("sequelize");
 const { name } = require("../package.json");
 
 //connect with the database
-console.log("this is link",process.env.POSTGRES_URL+`${name}`)
 const db = new Sequelize(process.env.POSTGRES_URL+`${name}`, {
     logging: false
 });
