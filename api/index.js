4aadf165ed2e76720b2ef396bb3b7e920275427e
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-18 16:17:25 -0400

    feat: Add the auth route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 // router.use('/register', require("../auth/register"));
 
+//already mounted on /api/ 
 router.use("/photos", require("./photos"));
 router.use("/users", require("./users"));
+router.use("/auth", require("./auth"));
 router.use((req, res, next) => {
     const error = new Error("404 Not Found");
     error.status = 404;
