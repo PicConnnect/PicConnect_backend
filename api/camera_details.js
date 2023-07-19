130226f4a1c3a3b02cb8fe64e36d2e8add23e061
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-07-19 13:43:49 +0800

    feat: added put request handling to handle changes in camera detail of a photo fix: temp disable routing to api/auth.js

--- api/camera_details.js
+++ api/camera_details.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const router = express.Router();
-const {Camera_Details} = require("../db/models");
+const {Camera_Details, Photo} = require("../db/models");
+// const photos = require ("./photos");
 
 router.post("/", async (req, res, next) => {
     const { make, model, exposure_time, aperture, focal_length, iso } = req.body;
@@ -10,9 +11,50 @@ router.post("/", async (req, res, next) => {
         });
 
         cameraDetail
+            ? res.status(200).json(cameraDetail.id, created)
+            : res.status(400).send("Error on creating or finding Camera Details");
             
         //const 
     } catch (error) {
         next(error);
     }
 });
+
+router.put("/:photoId", async (req, res, next) => {
+    const { make, model, exposure_time, aperture, focal_length, iso } = req.body;
+    const photoId = req.params.photoId;
+    const created = false;
+    try{
+        const cameraDetail = await Camera_Details.findOne({
+            where: {make, model, exposure_time, aperture, focal_length, iso}
+        });
+        const oldCameraDetailId = cameraDetail.id;
+        if(!cameraDetail) {
+            [cameraDetail, created] = await Camera_Details.findOrCreate({
+                where: {make, model, exposure_time, aperture, focal_length, iso}
+            });
+        }
+            const updatedPhoto = await Photo.update(
+                {cameraDetailId: cameraDetail.id},
+                {
+                    where: { id: photoId }
+                }
+        );
+        
+        const photoCount = await Photo.count({
+            where: {cameraDetailID: oldCameraDetailId}
+        });
+
+        if(photoCount === 0){
+            const deletedCameraDetail = await Camera_Details.destroy({
+                where: { id: oldCameraDetailId }
+            });
+            deletedCameraDetail
+                ? res.status(200).send("camera detail deleted")
+                : res.status(400).send("delete fail");
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 //already mounted on /api/ 
 router.use("/photos", require("./photos"));
 router.use("/users", require("./users"));
-router.use("/auth", require("./auth"));
+//router.use("/auth", require("./auth"));
 router.use((req, res, next) => {
     const error = new Error("404 Not Found");
     error.status = 404;
