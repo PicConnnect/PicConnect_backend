395bab66e5710cebd872d4d4ab9511444a266b77
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-11-05 17:50:50 -0500

    test: Add ssl configuration

--- db/db.js
+++ db/db.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 console.log(process.env.POSTGRES_URL);
-//connect with the database
+// Connect with the database
 const db = new Sequelize(process.env.POSTGRES_URL, {
   logging: false,
   dialectModule: require("pg"),
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  }
 });
 
 module.exports = db;
